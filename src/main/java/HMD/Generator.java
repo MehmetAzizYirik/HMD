1ea0151eab6e1973343f9562abd9c43131e44f07
Mehmet Aziz Yirik <mehmetazizyirik@outlook.com>
2019-05-28 13:50:43 +0200

    Update Generator.java

--- src/main/java/HMD/Generator.java
+++ src/main/java/HMD/Generator.java
@@ -1,3 +1,41 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2018 Mehmet Aziz Yirik
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+/**
+ * This class is a molecular structure generator project including also 
+ * re-implementation of some functions from Faulon's paper [1]. 
+ * 
+ * [1] Faulon, Jean Loup. "On using graph-equivalent classes for the structure
+ * elucidation of large molecules." Journal of chemical information and computer
+ * sciences 32.4 (1992): 338-348.
+ * 
+ * @author Mehmet Aziz Yirik
+ */
+
+
+
 package HMD;
 
 import java.io.FileWriter;
